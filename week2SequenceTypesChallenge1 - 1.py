8a677c0fb7959e202807cd4af53f2125b353058c
r426 <rutaslv@hotmail.lt>
2020-05-24 19:53:15 +0300

    Update week2SequenceTypesChallenge1 - 1.py

--- week2SequenceTypesChallenge1 - 1.py
+++ week2SequenceTypesChallenge1 - 1.py
@@ -3,7 +3,7 @@ Sequence Types Challenge 1
 
 Looks for the character in every unique(!) string in the list and,
 if found, counts the number of occurrences of that character
-in that string and appends the string that many times to the answer list
+in that string and appends the string that many times to the answer list.
 '''
 
 words = input("Enter a few words separated by spaces:")
